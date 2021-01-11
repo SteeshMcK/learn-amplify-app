38eb79367f3de7afd1ba80fa2fbb60e572316035
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-11 15:22:53 -0800

    add auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Question from './components/Question/Question'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
+  const pStyles = {
+    fontSize: '3em',
+    color: 'red'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <Question />
+        <p style={ pStyles }>We now have Auth!</p>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App)
